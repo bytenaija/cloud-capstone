aa0395ff4f7973d5179dbddb1fa246b3fe40e28a
bytenaija <everistusolumese@gmail.com>
2020-05-23 16:30:59 +0100

    modified eadme

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,5 +5,5 @@ export const authConfig = {
   // URL: Create an Auth0 application and copy values from it into this map
   domain: 'bytenaija.auth0.com', // Auth0 domain
   clientId: 'YGU6wCOp5VfZrRI9ppWBa8XmiqiYgDs7', // Auth0 client id
-  callbackUrl: 'http://localhost:3001/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
